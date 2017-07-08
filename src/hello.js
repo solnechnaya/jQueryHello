36eb27f2c92b721db1c15ee6ccf89c18a83cf490
Oleg Z <utmons@yandex.com>
2017-07-08 13:03:46 +0600

    Добавлен запрос к серверу

--- src/hello.js
+++ src/hello.js
@@ -4,18 +4,71 @@ $(document).ready(
         $('#submit').click(
             function () {
                 console.log('click');
-                const validUsername='Наташа';
-                const validPassword='qqq';
+                var validUsername;
+                var validPassword;
 
-                var username = $('#username').val();
-                var password = $('#password').val();
-                if (username == validUsername && password == validPassword) {
-                    alert("Ок");
-                } else {
-                    alert("Неверное имя пользователя или пароль");
-                }
+                $.ajax({
+                    url: 'http://localhost:8080/user',
+                    type: 'GET',
+                    dataType: 'json'
+                })
+                    .done(function (data) {
+
+                        validUsername = data.username;
+                        validPassword = data.password;
+
+                        var username = $('#username').val();
+                        var password = $('#password').val();
+                        if (username == validUsername && password == validPassword) {
+                            alert("Ок");
+                        } else {
+                            alert("Неверное имя пользователя или пароль");
+                        }
+
+                    })
+                    .fail(function () {
+                        alert("Ошибка сервера!")
+                    });
+
+
             }
         )
+        /*
+         $.ajax({
+         url: siteConfig.backEndUrl+ url + (id ? '/' + id : ''),
+         headers: { 'Content-Type': 'application/json', 'X-Swellby-SessionID': s_sessionId },
+         type: 'PUT',
+         dataType: 'json',
+         data: data ? JSON.stringify(data) : null
+         })
+         .done(function (data) {
+         success(data,triggerValue,closemodal,callback)
+         })
+         .fail(function (xhr, strErr) {
+         if(typeof errorcallback === "function") {
+         errorcallback(xhr, strErr);
+         } else {
+         errorHandler(xhr, strErr);
+         }
+         });
+
+         $.ajax({
+         url: siteConfig.backEndUrl+ url,
+         headers: { 'Content-Type': 'application/json', 'X-Swellby-SessionID': s_sessionId },
+         type: 'GET',
+         dataType: 'json'
+         })
+         .done(function (data) {
+         success(data,triggerValue,false,callback)
+         })
+         .fail(function (xhr, strErr) {
+         errorHandler(xhr, strErr);
+         if(typeof errorcallback === "function") {
+         errorcallback();
+         }
+         });
+
+         */
     }
 );
 
