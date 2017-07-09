57fd624a78d2fb71292b4559d66fce52cd66aa0f
Oleg Z <utmons@yandex.com>
2017-07-09 18:24:30 +0600

    interim

--- src/hello.js
+++ src/hello.js
@@ -1,74 +1,58 @@
 console.log('hello');
-$(document).ready(
-    function () {
-        $('#submit').click(
-            function () {
-                console.log('click');
-                var validUsername;
-                var validPassword;
-
-                $.ajax({
-                    url: 'http://localhost:8080/user',
-                    type: 'GET',
-                    dataType: 'json'
-                })
-                    .done(function (data) {
-
-                        validUsername = data.username;
-                        validPassword = data.password;
-
-                        var username = $('#username').val();
-                        var password = $('#password').val();
-                        if (username == validUsername && password == validPassword) {
-                            alert("Ок");
-                        } else {
-                            alert("Неверное имя пользователя или пароль");
-                        }
-
-                    })
-                    .fail(function () {
-                        alert("Ошибка сервера!")
-                    });
 
 
+function showName(data) {
+    $('#name').html(data.name + ' ' + data.lastName);
+}
+
+function getName() {
+    $.ajax({
+        url: 'http://localhost:8080/username',
+        type: 'GET',
+        dataType: 'json'
+    })
+        .done(function(data) {showName(data);})
+        .fail(function () {
+            alert("Ошибка сервера!")
+        });
+}
+
+function login(data) {
+    var validUsername = data.username;
+    var validPassword = data.password;
+
+    var username = $('#username').val();
+    var password = $('#password').val();
+
+    console.log(data.username + " " + data.password);
+
+    if (username == validUsername && password == validPassword) {
+        getName();
+    } else {
+        alert("Неверное имя пользователя или пароль");
+    }
+}
+
+function submit() {
+    console.log('click');
+
+    $.ajax({
+        url: 'http://localhost:8080/user',
+        type: 'GET',
+        dataType: 'json'
+    })
+        .done(function (data) {
+                login(data);
             }
         )
-        /*
-         $.ajax({
-         url: siteConfig.backEndUrl+ url + (id ? '/' + id : ''),
-         headers: { 'Content-Type': 'application/json', 'X-Swellby-SessionID': s_sessionId },
-         type: 'PUT',
-         dataType: 'json',
-         data: data ? JSON.stringify(data) : null
-         })
-         .done(function (data) {
-         success(data,triggerValue,closemodal,callback)
-         })
-         .fail(function (xhr, strErr) {
-         if(typeof errorcallback === "function") {
-         errorcallback(xhr, strErr);
-         } else {
-         errorHandler(xhr, strErr);
-         }
-         });
+        .fail(function () {
+            alert("Ошибка сервера!")
+        });
+}
 
-         $.ajax({
-         url: siteConfig.backEndUrl+ url,
-         headers: { 'Content-Type': 'application/json', 'X-Swellby-SessionID': s_sessionId },
-         type: 'GET',
-         dataType: 'json'
-         })
-         .done(function (data) {
-         success(data,triggerValue,false,callback)
-         })
-         .fail(function (xhr, strErr) {
-         errorHandler(xhr, strErr);
-         if(typeof errorcallback === "function") {
-         errorcallback();
-         }
-         });
-
-         */
+$(document).ready(
+    function () {
+        $('#submit').click(submit);
     }
 );
 
